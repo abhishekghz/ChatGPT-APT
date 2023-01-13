cefd4a12552a44f9fef704bfdd18a64dc03acaed
Abhishek Gautam <Gautam.abhishek7100@gmail.com>
2023-01-13 22:35:17 +0530

    updated commit

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://abhishek-kwkf.onrender.com', {
+    const response = await fetch('https://abhishek-7di7.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
